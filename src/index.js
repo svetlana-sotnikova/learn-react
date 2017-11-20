0de7844bad06868b91e7cc4c8efbcf840b66f062
Svetlana Troshina <svetlana.data@gmail.com>
2017-11-20 22:37:59 +0300

    вернулась к hello world

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker()
 
-setInterval(tick, 1000);
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
